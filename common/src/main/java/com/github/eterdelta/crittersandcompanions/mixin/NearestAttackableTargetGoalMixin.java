df8db1f5771caee823af7774cc40726a263bc2fd
Niklas Widmann <nik@somethingcatchy.net>
2025-06-17 11:44:03 +0200

    shadow is evil

--- common/src/main/java/com/github/eterdelta/crittersandcompanions/mixin/NearestAttackableTargetGoalMixin.java
+++ common/src/main/java/com/github/eterdelta/crittersandcompanions/mixin/NearestAttackableTargetGoalMixin.java
@@ -10,7 +10,7 @@ import net.minecraft.world.entity.ai.targeting.TargetingConditions;
 import net.minecraft.world.entity.monster.Drowned;
 import net.minecraft.world.entity.monster.Guardian;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -18,8 +18,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(NearestAttackableTargetGoal.class)
 public abstract class NearestAttackableTargetGoalMixin<T extends LivingEntity> extends TargetGoal {
 
-    @Shadow
-    protected TargetingConditions targetConditions;
+    @Accessor
+    abstract TargetingConditions getTargetConditions();
 
     protected NearestAttackableTargetGoalMixin(Mob mob, boolean mustSee) {
         super(mob, mustSee);
@@ -28,7 +28,7 @@ public abstract class NearestAttackableTargetGoalMixin<T extends LivingEntity> e
     @Inject(at = @At("RETURN"), method = "<init>(Lnet/minecraft/world/entity/Mob;Ljava/lang/Class;IZZLjava/util/function/Predicate;)V")
     private void onInit(Mob mob, Class<T> targetType, int randomInterval, boolean mustSee, boolean mustReach, Predicate<LivingEntity> targetPredicate, CallbackInfo callback) {
         if (mob instanceof Drowned || mob instanceof Guardian) {
-            this.targetConditions.selector(targetPredicate != null
+            getTargetConditions().selector(targetPredicate != null
                     ? targetPredicate.and(target -> PearlNecklaceItem.getWearing(target)
                     .map(PearlNecklaceItem::getLevel)
                     .map(level -> getFollowDistance() - (getFollowDistance() * ((level * 20) / 100.0F)))
