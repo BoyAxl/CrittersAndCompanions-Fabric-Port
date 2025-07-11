03bbfb54d01408aecf42e293705ac7c52957a80d
Niklas Widmann <nik@somethingcatchy.net>
2025-07-11 13:52:19 +0200

    rework otter pathfinding a bit to make the not get stuck so much

--- neoforge/build.gradle.kts
+++ neoforge/build.gradle.kts
@@ -3,6 +3,7 @@ val geckolib_version: String by extra
 val easy_villager_version: String by extra
 val jei_version: String by extra
 val curios_version: String by extra
+val pathfinding_renderer_version: String by extra
 
 neoforge {
     dependOn(project(":common"))
@@ -16,4 +17,6 @@ dependencies {
 
     modRuntimeOnly("maven.modrinth:easy-villagers:${easy_villager_version}")
     modRuntimeOnly("mezz.jei:jei-${mc_version}-neoforge:${jei_version}")
+
+    modRuntimeOnly("com.possible-triangle:pathfinding_renderer-neoforge:${mc_version}-${pathfinding_renderer_version}")
 }
